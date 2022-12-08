0e30ffb6a0f9840377404c95c556dabd2c2ae417
Aymen <AymenMir1001@gmail.com>
2022-12-08 22:23:10 +0100

    Update PostPage.js

--- front-end/src/pages/PostPage.js
+++ front-end/src/pages/PostPage.js
@@ -1,20 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useParams, useNavigate } from 'react-router-dom';
-import MDEditor from '@uiw/react-md-editor';
+import { useParams, useNavigate } from "react-router-dom";
+import MDEditor from "@uiw/react-md-editor";
 import { AiOutlineTwitter, AiOutlineGithub } from "react-icons/ai";
 import { RiDiscordFill } from "react-icons/ri";
 import { ethers } from "ethers";
-import axios from 'axios';
+import axios from "axios";
 
+import { IPFS_GATEWAY } from "./../utils/ipfsStorage";
 import Blog from "../artifacts/MyBlog.sol/MyBlog.json";
-import { contractAddress, networkDeployedTo, ownerAddress } from "../utils/contracts-config";
+import {
+  contractAddress,
+  networkDeployedTo,
+  ownerAddress,
+} from "../utils/contracts-config";
 import networksMap from "../utils/networksMap.json";
 
 function PostPage() {
-  const { id } = useParams()
-  const navigate = useNavigate()
-  const data = useSelector((state) => state.blockchain.value)
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const data = useSelector((state) => state.blockchain.value);
 
   const [post, setPost] = useState({
     id: Number(id),
@@ -23,106 +28,125 @@ function PostPage() {
     coverImg: "",
     readTime: 0,
     createdAt: "",
-    type: 1
-  })
+    type: 1,
+  });
   async function getPostDetails() {
     if (data.network === networksMap[networkDeployedTo]) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum,
+        "any"
+      );
       const blog = new ethers.Contract(contractAddress, Blog.abi, provider);
 
-      const owner = await blog.callStatic.owner()
+      const owner = await blog.callStatic.owner();
 
-      let _isMember
+      let _isMember;
       if (owner === data.account) {
-        _isMember = true
+        _isMember = true;
       } else {
-        _isMember = await blog.isMember(data.account)
+        _isMember = await blog.isMember(data.account);
       }
-      const _post = (await blog.getAllPosts())[Number(id)]
+      const _post = (await blog.getAllPosts())[Number(id)];
       if (_isMember && _post[7] !== 0) {
-        const _posts = (await axios.get("http://localhost:3001/")).data
-        const _postContent = (await axios.get(_posts[String(Number(_post[0]))])).data
+        const imageUrl = _post[3].replace("ipfs://", IPFS_GATEWAY);
+        const _posts = (await axios.get("http://localhost:3001/")).data;
+        const contentUrl = _posts[String(Number(_post[0]))].replace(
+          "ipfs://",
+          IPFS_GATEWAY
+        );
+        const _postContent = (await axios.get(contentUrl)).data.content;
         setPost({
           ...post,
           id: Number(_post[0]),
           title: _post[1],
-          coverImg: _post[3],
+          coverImg: imageUrl,
           readTime: Number(_post[4]),
           content: _postContent,
           createdAt: new Date(Number(_post[6]) * 1000),
-          type: _post[7]
-        })
+          type: _post[7],
+        });
       } else if (_post[7] === 0) {
-        const metaData = await axios.get(_post[5])
+        const imageUrl = _post[3].replace("ipfs://", IPFS_GATEWAY);
+        const contentUrl = _post[5].replace("ipfs://", IPFS_GATEWAY);
+        let metaData = await axios.get(contentUrl);
         setPost({
           ...post,
           id: Number(_post[0]),
           title: _post[1],
-          coverImg: _post[3],
+          coverImg: imageUrl,
           readTime: Number(_post[4]),
-          content: metaData.data,
+          content: metaData.data.content,
           createdAt: new Date(Number(_post[6]) * 1000),
-          type: _post[7]
-        })
+          type: _post[7],
+        });
       } else {
-        navigate("/subscribe")
+        navigate("/subscribe");
       }
     }
   }
 
   useEffect(() => {
     if (window.ethereum !== undefined) {
-      getPostDetails()
+      getPostDetails();
     }
-  }, [data.network])
+  }, [data.network]);
 
   return (
     <div>
-      <section className='post-header'>
-        <div className='header-content post-container'>
+      <section className="post-header">
+        <div className="header-content post-container">
           {data.account === ownerAddress ? (
-            <a href={"/edit-post/" + String(post.id)} className='back-home'> Edit Post</a>
+            <a href={"/edit-post/" + String(post.id)} className="back-home">
+              {" "}
+              Edit Post
+            </a>
           ) : (
-            <a href='/' className='back-home'> Back Home</a>
+            <a href="/" className="back-home">
+              {" "}
+              Back Home
+            </a>
           )}
 
-          <h1 className='header-title'>{post.title}</h1>
-          <img src={post.coverImg} className='header-img' />
+          <h1 className="header-title">{post.title}</h1>
+          <img src={post.coverImg} className="header-img" />
         </div>
       </section>
-      <section className='post-content post-container' data-color-mode="light">
-        <MDEditor.Markdown source={post.content} style={{ whiteSpace: 'pre-wrap' }} />
+      <section className="post-content post-container" data-color-mode="light">
+        <MDEditor.Markdown
+          source={post.content}
+          style={{ whiteSpace: "pre-wrap" }}
+        />
       </section>
-      <div className='share post-container'>
-        <span className='share-title'>Share this article</span>
-        <div className='social'>
-          <a href='https://github.com/Aymen1001' className='bx'>
+      <div className="share post-container">
+        <span className="share-title">Share this article</span>
+        <div className="social">
+          <a href="https://github.com/Aymen1001" className="bx">
             <AiOutlineGithub size={25} />
           </a>
-          <a href='https://github.com/Aymen1001' className='bx'>
+          <a href="https://github.com/Aymen1001" className="bx">
             <AiOutlineTwitter size={25} />
           </a>
-          <a href='https://github.com/Aymen1001' className='bx'>
+          <a href="https://github.com/Aymen1001" className="bx">
             <RiDiscordFill size={25} />
           </a>
         </div>
       </div>
-      <div className='footer container'>
+      <div className="footer container">
         <p>&#169; All Right Reserved</p>
-        <div className='social'>
-          <a href='https://github.com/Aymen1001' className='bx'>
+        <div className="social">
+          <a href="https://github.com/Aymen1001" className="bx">
             <AiOutlineGithub size={25} />
           </a>
-          <a href='https://github.com/Aymen1001' className='bx'>
+          <a href="https://github.com/Aymen1001" className="bx">
             <AiOutlineTwitter size={25} />
           </a>
-          <a href='https://github.com/Aymen1001' className='bx'>
+          <a href="https://github.com/Aymen1001" className="bx">
             <RiDiscordFill size={25} />
           </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default PostPage
+export default PostPage;
