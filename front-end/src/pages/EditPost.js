b701f9cb889b92ed984fa30db0916acc26cbefa3
Aymen <AymenMir1001@gmail.com>
2022-12-08 22:25:39 +0100

    Update EditPost.js

--- front-end/src/pages/EditPost.js
+++ front-end/src/pages/EditPost.js
@@ -1,256 +1,311 @@
 import "../assets/styles.css";
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useSelector } from "react-redux"
-import { Form } from "react-bootstrap"
-import { CircularProgress } from "@mui/material"
+import { useSelector } from "react-redux";
+import { Form } from "react-bootstrap";
+import { CircularProgress } from "@mui/material";
 import { ethers } from "ethers";
-import axios from 'axios';
-import { Buffer } from "buffer";
-import { create } from "ipfs-http-client";
-import MDEditor from '@uiw/react-md-editor';
+import axios from "axios";
+import MDEditor from "@uiw/react-md-editor";
+import { File } from "web3.storage";
 
+import { ipfsSaveContent, IPFS_GATEWAY } from "./../utils/ipfsStorage";
 import Blog from "../artifacts/MyBlog.sol/MyBlog.json";
 import { contractAddress, networkDeployedTo } from "../utils/contracts-config";
 import networksMap from "../utils/networksMap.json";
 
-
-const ipfsClient = create("https://ipfs.infura.io:5001/api/v0")
-const ipfsBaseUrl = "https://ipfs.infura.io/ipfs/"
-
 const EditPost = () => {
-    const { id } = useParams()
-    let navigate = useNavigate();
-    const data = useSelector((state) => state.blockchain.value)
+  const { id } = useParams();
+  let navigate = useNavigate();
+  const data = useSelector((state) => state.blockchain.value);
 
-    const [loading, setLoading] = useState(false)
-    const [image, setImage] = useState(null)
-    const [content, setContent] = useState("")
-    const [isOwner, setIsOwner] = useState(false)
-    const [formInput, setFormInput] = useState({ title: "", overview: "", coverImg: "", readTime: "" })
-    const [post, setPost] = useState({ id: 0, title: "", overview: "", coverImg: "", readTime: "", content: "", postType: 0 })
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState({
+    name: "",
+    file: null,
+  });
+  const [content, setContent] = useState("");
+  const [isOwner, setIsOwner] = useState(false);
+  const [formInput, setFormInput] = useState({
+    title: "",
+    overview: "",
+    coverImg: "",
+    readTime: "",
+  });
+  const [post, setPost] = useState({
+    id: 0,
+    title: "",
+    overview: "",
+    coverImg: "",
+    readTime: "",
+    content: "",
+    contentURI: "",
+    postType: 0,
+  });
 
-    const getImage = async (e) => {
+  const getImage = async (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    setImage({
+      name: file.name,
+      file: file,
+    });
+    setFormInput({ ...formInput, coverImg: URL.createObjectURL(file) });
+  };
 
-        e.preventDefault()
-        const reader = new window.FileReader();
-        const file = e.target.files[0];
+  async function getPostDetails() {
+    if (data.network === networksMap[networkDeployedTo]) {
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum,
+        "any"
+      );
+      const blog = new ethers.Contract(contractAddress, Blog.abi, provider);
+      const _post = (await blog.getAllPosts())[Number(id)];
 
-        if (file !== undefined) {
-            reader.readAsArrayBuffer(file)
+      if (_post[7] === 0) {
+        const imageUrl = _post[3].replace("ipfs://", IPFS_GATEWAY);
+        const contentUrl = _post[5].replace("ipfs://", IPFS_GATEWAY);
+        let metaData = await axios.get(contentUrl);
+        setPost({
+          ...post,
+          id: Number(_post[0]),
+          title: _post[1],
+          overview: _post[2],
+          coverImg: imageUrl,
+          readTime: Number(_post[4]),
+          contentURI: _post[5],
+          content: metaData.data.content,
+          postType: _post[7],
+        });
+        setFormInput({
+          ...formInput,
+          title: _post[1],
+          overview: _post[2],
+          coverImg: imageUrl,
+          readTime: Number(_post[4]),
+        });
+        setContent(metaData.data.content);
+      } else if (_post[7] === 1) {
+        const _posts = (await axios.get("http://localhost:3001/")).data;
+        const contentUrl = _posts[String(Number(_post[0]))].replace(
+          "ipfs://",
+          IPFS_GATEWAY
+        );
+        const _postContent = (await axios.get(contentUrl)).data.content;
 
-            reader.onloadend = () => {
-                const buf = Buffer(reader.result, "base64")
-                setImage(buf)
-                setFormInput({ ...formInput, coverImg: URL.createObjectURL(file) })
-            }
-        }
+        setPost({
+          ...post,
+          id: Number(_post[0]),
+          title: _post[1],
+          overview: _post[2],
+          coverImg: _post[3],
+          readTime: Number(_post[4]),
+          contentURI: contentUrl,
+          content: _postContent,
+          postType: _post[7],
+        });
+        setFormInput({
+          ...formInput,
+          title: _post[1],
+          overview: _post[2],
+          coverImg: _post[3],
+          readTime: Number(_post[4]),
+        });
+        setContent(_postContent);
+      }
     }
+  }
 
-    async function getPostDetails() {
-        if (data.network === networksMap[networkDeployedTo]) {
-            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-            const blog = new ethers.Contract(contractAddress, Blog.abi, provider);
-            const _post = (await blog.getAllPosts())[Number(id)]
+  async function edit() {
+    if (data.network === networksMap[networkDeployedTo]) {
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum,
+        "any"
+      );
+      const signer = provider.getSigner();
+      const blog = new ethers.Contract(contractAddress, Blog.abi, signer);
 
-            if (_post[7] === 0) {
-                const metaData = await axios.get(_post[5])
-                setPost({
-                    ...post,
-                    id: Number(_post[0]),
-                    title: _post[1],
-                    overview: _post[2],
-                    coverImg: _post[3],
-                    readTime: Number(_post[4]),
-                    contentURI: _post[5],
-                    content: metaData.data,
-                    postType: _post[7]
-                })
-                setFormInput({
-                    ...formInput,
-                    title: _post[1],
-                    overview: _post[2],
-                    coverImg: _post[3],
-                    readTime: Number(_post[4])
-                })
-                setContent(metaData.data)
-            } else if (_post[7] === 1) {
-                const _posts = (await axios.get("http://localhost:3001/")).data
-                console.log(await axios.get("http://localhost:3001/"))
-                const _postContent = (await axios.get(_posts[String(Number(_post[0]))])).data
-
-                setPost({
-                    ...post,
-                    id: Number(_post[0]),
-                    title: _post[1],
-                    overview: _post[2],
-                    coverImg: _post[3],
-                    readTime: Number(_post[4]),
-                    contentURI: _posts[String(Number(_post[0]))],
-                    content: _postContent,
-                    postType: _post[7]
-                })
-                setFormInput({
-                    ...formInput,
-                    title: _post[1],
-                    overview: _post[2],
-                    coverImg: _post[3],
-                    readTime: Number(_post[4])
-                })
-                setContent(_postContent)
-            }
-        }
-    }
-
-    async function edit() {
-        if (data.network === networksMap[networkDeployedTo]) {
-            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-            const signer = provider.getSigner()
-            const blog = new ethers.Contract(contractAddress, Blog.abi, signer);
-
-            try {
-                setLoading(true)
-                let imageURI;
-                if (formInput.coverImg !== post.coverImg) {
-                    const addedFile = await ipfsClient.add(image)
-                    imageURI = ipfsBaseUrl + addedFile.path;
-                } else {
-                    imageURI = post.coverImg;
-                }
-
-                let contentURI;
-                if (content !== post.content) {
-                    const addedContent = await ipfsClient.add(content)
-                    contentURI = ipfsBaseUrl + addedContent.path
-                } else {
-                    contentURI = post.contentURI;
-                }
-
-                let contentHash;
-                if (post.postType == 0) {
-                    contentHash = contentURI;
-                } else if (post.postType == 1) {
-                    contentHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(contentURI))
-                    axios.post("http://localhost:3001/save-post", {
-                        id: post.id,
-                        post: contentURI
-                    })
-                }
-
-                const edit_tx = await blog.updatePost(
-                    Number(id),
-                    formInput.title,
-                    formInput.overview,
-                    imageURI,
-                    Number(formInput.readTime),
-                    contentHash
-                )
-                await edit_tx.wait();
-
-                setImage(null)
-                setLoading(false)
-                navigate("/post/" + id)
-            }
-            catch (err) {
-                setLoading(false)
-                window.alert("An error has occured")
-                console.log(err)
-            }
+      try {
+        setLoading(true);
+        let imageURI;
+        if (formInput.coverImg !== post.coverImg) {
+          const cid = await ipfsSaveContent(image.file);
+          imageURI = `ipfs://${cid}/${image.name}`;
         } else {
-            window.alert(`Please Switch to the ${networksMap[networkDeployedTo]} network`)
+          imageURI = post.coverImg;
         }
-    }
-    async function getBlogOwner() {
-        if (data.account !== "") {
-            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-            const signer = provider.getSigner()
-            const blog = new ethers.Contract(contractAddress, Blog.abi, signer);
 
-            const owner = await blog.callStatic.owner()
+        let contentURI;
+        if (content !== post.content) {
+          const jsonContent = JSON.stringify({
+            content: content,
+          });
 
-            if (owner == data.account) {
-                setIsOwner(true)
-            } else {
-                navigate("/")
-            }
+          const blob = new Blob([jsonContent], {
+            type: "application/json",
+          });
+
+          const file = new File([blob], "postContent.json");
+
+          const dataCid = await ipfsSaveContent(file);
+          contentURI = `ipfs://${dataCid}/postContent.json`;
         } else {
-            navigate("/")
+          contentURI = post.contentURI;
         }
+
+        let contentHash;
+        if (post.postType == 0) {
+          contentHash = contentURI;
+        } else if (post.postType == 1) {
+          contentHash = ethers.utils.keccak256(
+            ethers.utils.toUtf8Bytes(contentURI)
+          );
+          axios.post("http://localhost:3001/save-post", {
+            id: post.id,
+            post: contentURI,
+          });
+        }
+
+        const edit_tx = await blog.updatePost(
+          Number(id),
+          formInput.title,
+          formInput.overview,
+          imageURI,
+          Number(formInput.readTime),
+          contentHash
+        );
+        await edit_tx.wait();
+
+        setImage({ name: "", file: null });
+        setLoading(false);
+        navigate("/post/" + id);
+      } catch (err) {
+        setLoading(false);
+        window.alert("An error has occured");
+        console.log(err);
+      }
+    } else {
+      window.alert(
+        `Please Switch to the ${networksMap[networkDeployedTo]} network`
+      );
     }
+  }
+  async function getBlogOwner() {
+    if (data.account !== "") {
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum,
+        "any"
+      );
+      const signer = provider.getSigner();
+      const blog = new ethers.Contract(contractAddress, Blog.abi, signer);
 
-    useEffect(() => {
-        if (window.ethereum !== undefined) {
-            getBlogOwner()
-            getPostDetails()
-        }
-    }, [data.network])
+      const owner = await blog.callStatic.owner();
 
+      if (owner == data.account) {
+        setIsOwner(true);
+      } else {
+        navigate("/");
+      }
+    } else {
+      navigate("/");
+    }
+  }
 
-    return (
-        isOwner ? (
-            <>
-                <div className="mainheading">
-                    <h1 className="sitetitle">Edit Your post</h1>
-                </div>
+  useEffect(() => {
+    if (window.ethereum !== undefined) {
+      getBlogOwner();
+      getPostDetails();
+    }
+  }, [data.network]);
 
-                <div className="container" style={{ display: "grid", justifyContent: "center", alignItems: "center", textAlign: "left", paddingTop: "4rem", paddingBottom: "4rem" }}>
+  return isOwner ? (
+    <>
+      <div className="mainheading">
+        <h1 className="sitetitle">Edit Your post</h1>
+      </div>
 
-                    <div style={{ width: "500px" }}>
-                        <label>Post title : </label>
-                        <Form.Control
-                            type="text"
-                            maxLength={80}
-                            onChange={(e) => setFormInput({ ...formInput, title: e.target.value })}
-                            value={formInput.title} />
-                    </div>
-                    <br />
-                    <div style={{ width: "500px" }}>
-                        <label>Post overview : </label>
-                        <Form.Control type="text" as="textarea"
-                            rows={4}
-                            maxLength={150}
-                            onChange={(e) => setFormInput({ ...formInput, overview: e.target.value })}
-                            value={formInput.overview} />
-                    </div>
-                    <br />
-                    <div style={{ width: "500px" }}>
-                        <label>Post read time (in minutes) : </label>
-                        <Form.Control type="Number"
-                            onChange={(e) => setFormInput({ ...formInput, readTime: e.target.value })}
-                            value={formInput.readTime} />
-                    </div>
-                    <br />
-                    <div style={{ width: "1000px" }} data-color-mode="light">
-                        <label>Post content: </label>
-                        <MDEditor
-                            height={400}
-                            value={content}
-                            onChange={setContent}
-                        />
-                    </div>
-                    <br />
-                    <div style={{ width: "500px" }}>
-                        <label>Change your cover image : </label>
-                        <Form.Control type="file" name="file" onChange={(e) => { getImage(e) }} />
-                    </div>
-                    <br />
-                    {
-                        formInput.coverImg && (
-                            <div style={{ textAlign: "center", width: "350px" }}>
-                                <img className="rounded mt-4" src={formInput.coverImg} />
-                            </div>
-                        )
-                    }
-                    <br />
-                    <div className="container" style={{ textAlign: "center" }}>
-                        <a className="login" onClick={edit} role="button">
-                            {loading ? <CircularProgress color="inherit" size={20} /> : "Update"}
-                        </a>
-                    </div>
-                </div>
-            </>
-        ) : null
-    );
+      <div
+        className="container"
+        style={{
+          display: "grid",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "left",
+          paddingTop: "4rem",
+          paddingBottom: "4rem",
+        }}
+      >
+        <div style={{ width: "500px" }}>
+          <label>Post title : </label>
+          <Form.Control
+            type="text"
+            maxLength={80}
+            onChange={(e) =>
+              setFormInput({ ...formInput, title: e.target.value })
+            }
+            value={formInput.title}
+          />
+        </div>
+        <br />
+        <div style={{ width: "500px" }}>
+          <label>Post overview : </label>
+          <Form.Control
+            type="text"
+            as="textarea"
+            rows={4}
+            maxLength={150}
+            onChange={(e) =>
+              setFormInput({ ...formInput, overview: e.target.value })
+            }
+            value={formInput.overview}
+          />
+        </div>
+        <br />
+        <div style={{ width: "500px" }}>
+          <label>Post read time (in minutes) : </label>
+          <Form.Control
+            type="Number"
+            onChange={(e) =>
+              setFormInput({ ...formInput, readTime: e.target.value })
+            }
+            value={formInput.readTime}
+          />
+        </div>
+        <br />
+        <div style={{ width: "1000px" }} data-color-mode="light">
+          <label>Post content: </label>
+          <MDEditor height={400} value={content} onChange={setContent} />
+        </div>
+        <br />
+        <div style={{ width: "500px" }}>
+          <label>Change your cover image : </label>
+          <Form.Control
+            type="file"
+            name="file"
+            onChange={(e) => {
+              getImage(e);
+            }}
+          />
+        </div>
+        <br />
+        {formInput.coverImg && (
+          <div style={{ textAlign: "center", width: "350px" }}>
+            <img className="rounded mt-4" src={formInput.coverImg} />
+          </div>
+        )}
+        <br />
+        <div className="container" style={{ textAlign: "center" }}>
+          <a className="login" onClick={edit} role="button">
+            {loading ? (
+              <CircularProgress color="inherit" size={20} />
+            ) : (
+              "Update"
+            )}
+          </a>
+        </div>
+      </div>
+    </>
+  ) : null;
 };
 
 export default EditPost;
