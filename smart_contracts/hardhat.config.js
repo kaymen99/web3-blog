2315648a699ac1144de3e24cb5c33feb40266f51
Aymen <AymenMir1001@gmail.com>
2023-02-11 23:19:51 +0100

    Update hardhat.config.js

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -19,10 +19,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const RINKEBY_ETHERSCAN_API_KEY = process.env.RINKEBY_ETHERSCAN_API_KEY
-const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL
-const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
+const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL;
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
 
 module.exports = {
   solidity: {
@@ -32,20 +31,15 @@ module.exports = {
       }
     ],
   },
-  networks: {
+    networks: {
     hardhat: {
-      chainId: 31337
+      chainId: 31337,
     },
     ganache: {
       chainId: 1337,
       url: "http://127.0.0.1:7545",
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: [process.env.PRIVATE_KEY],
     },
-    // rinkeby: {
-    //   url: RINKEBY_RPC_URL,
-    //   accounts: [process.env.PRIVATE_KEY],
-    //   chainId: 4,
-    // }
     // mumbai: {
     //   url: MUMBAI_RPC_URL,
     //   accounts: [process.env.PRIVATE_KEY],
@@ -58,11 +52,12 @@ module.exports = {
     // }
   },
   paths: {
-    artifacts: "./artifacts"
+    artifacts: "./artifacts",
+  },
+  gasReporter: {
+    enabled: true,
   },
   etherscan: {
-    apiKey: {
-      rinkeby: RINKEBY_ETHERSCAN_API_KEY,
-    }
+    apiKey: POLYGONSCAN_API_KEY,
   }
 };
