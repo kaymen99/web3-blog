c03bc78b023057eafa5560b09d3c092f0df91205
Aymen <AymenMir1001@gmail.com>
2023-02-23 20:57:29 +0100

    Update Footer.js

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -7,13 +7,13 @@ function Footer() {
         <div className='footer container'>
             <p>&#169; All Right Reserved</p>
             <div className='social'>
-                <a href='https://github.com/Aymen1001' className='bx'>
+                <a href='https://github.com/kaymen99' className='bx'>
                     <AiOutlineGithub size={25} />
                 </a>
-                <a href='https://github.com/Aymen1001' className='bx'>
+                <a href='https://github.com/kaymen99' className='bx'>
                     <AiOutlineTwitter size={25} />
                 </a>
-                <a href='https://github.com/Aymen1001' className='bx'>
+                <a href='https://github.com/kaymen99' className='bx'>
                     <RiDiscordFill size={25} />
                 </a>
             </div>
